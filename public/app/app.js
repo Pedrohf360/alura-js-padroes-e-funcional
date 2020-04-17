54c207570b4dafeccbf7bf03768153e970c46561
pedrohf360 <pedro.henriqueff10@gmail.com>
2020-04-17 11:09:14 -0300

    Adding log func in prmise helpers; Adding reduce to get total value

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,13 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus,  log } from './utils/promise-helpers.js';
 
 document
-.querySelector('#myButton')
-.onclick = () => 
-    fetch('http://localhost:3000/notas')
-    .then(handleStatus)
-    .then(notas => console.log(notas))
-    .catch(err => console.log(err));
+    .querySelector('#myButton')
+    .onclick = () =>
+        fetch('http://localhost:3000/notas')
+            .then(handleStatus)
+            .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+            .then(itens => itens.filter(item => item.codigo === '2143'))
+            .then(log)
+            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+            .then(log)
+            .catch(err => console.log(err));
